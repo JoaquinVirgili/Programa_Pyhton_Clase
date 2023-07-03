890aff54ea66a20cae38561573109d03743b9e18
GabexAR <aronson.gabriel.g@gmail.com>
2023-07-03 20:08:51 -0300

    Update ContactForUpdate.py
    Se modifico el contacts for update  para que el valor de birthday sea por defecto nulo, ya que si es vacio genera un error al no ingresar ningun dato.

--- Model/DTO/ContactForUpdate.py
+++ Model/DTO/ContactForUpdate.py
@@ -2,16 +2,16 @@ class ContactForUpdate:
     id = 0
     name = ""
     surname = ""
-    fechaNacimiento = ""
     email = ""
     username = ""
     state = 1
+    birthday = None
 
-    def __init__(self, id, name, surname, fechaNacimiento, email, username) -> None:
+    def __init__(self, id, name, surname, email, username) -> None:
         self.id = id
         self.name = name
         self.surname = surname
-        self.fechaNacimiento = fechaNacimiento
         self.email = email
         self.username = username
         self.state = 1
+        self.birthday = None
