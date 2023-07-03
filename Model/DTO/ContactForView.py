645995633791cb913406ad28297d206faeb5b7ed
GabexAR <aronson.gabriel.g@gmail.com>
2023-07-03 20:09:16 -0300

    Update ContactForView.py
    Se ajusto el fecha de nacimiento con los valores que tengo en mi base de datos.

--- Model/DTO/ContactForView.py
+++ Model/DTO/ContactForView.py
@@ -2,19 +2,19 @@ class ContactForView:
     id= 0
     name = ""
     surname = ""
-    fechaNacimiento = ""
     email = ""
     username = ""
+    birthday = None
 
     def __init__(self, contact) -> None:
         self.name = contact.name
         self.surname = contact.surname
-        self.fechaNacimiento = contact.fechaNacimiento
+        self.birthday = contact.birthday
         self.email = contact.email
         self.username = contact.username
         self.id = contact.id
 
     def __str__(self) -> str:
-        return f"Nombre: {self.name} - Apellido: {self.surname} - Fecha Nacimiento: {self.fechaNacimiento} - Email: {self.email} - Id: {self.id}"
+        return f"Nombre: {self.name} - Apellido: {self.surname} - Fecha Nacimiento: {self.birthday} - Email: {self.email} - Id: {self.id}"
 
     
