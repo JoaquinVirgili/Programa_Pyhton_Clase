18a6a33003130ab7ec0212541530ed88b14756cc
GabexAR <aronson.gabriel.g@gmail.com>
2023-07-03 20:11:10 -0300

    Update ContactRepository.py
    Se modificaron los query para que traigan todos los valores y que formate la fecha despues de traerla, ademas hay una condicion de que si la fecha esta vacia la deje como esta. Tambien se modifico por si trae datos de la lista de cumpleaños y esta vacia para que no de error.

--- Model/Repository/ContactRepository.py
+++ Model/Repository/ContactRepository.py
@@ -5,47 +5,58 @@ from datetime import datetime
 class ContactRepository:
 
     def get_contacts_by_user(self, username):
-        with Context() as context1:
-            query = "SELECT *, DATE_FORMAT(fechaNacimiento, '%d-%m-%Y') AS fechaNacimiento_invertida FROM contact WHERE state = 1 AND username = %s"
+        with Context() as context1:            
+            query = "SELECT * FROM contact WHERE state = 1 && username = %s "
             values = (username,)
             context1.mycursor.execute(query,values)
             contactsDB = context1.mycursor.fetchall()
         contacts = []
         for contactDB in contactsDB:
             contact = Contact()
+           
             contact.id = contactDB[0]
             contact.name = contactDB[1]
             contact.surname = contactDB[2]
-            contact.email = contactDB[3]
+            contact.email = contactDB[3]            
             contact.username = contactDB[4]
             contact.state = contactDB[5]
-            contact.fechaNacimiento = contactDB[7]
+            date_str = contactDB[6] 
+            if not date_str == None:
+                contact.birthday = date_str.strftime('%d-%m-%Y')
+            else:
+                contact.birthday = date_str
             contacts.append(contact)
         return contacts
     
     def get_contacts_birthday(self, username):
         fecha_actual = datetime.now().month        
         with Context() as context1:
-            query = "SELECT *, DATE_FORMAT(fechaNacimiento, '%d-%m-%Y') AS fechaNacimiento_invertida FROM contact WHERE state = 1 AND username = %s AND MONTH(fechaNacimiento) = %s"
+            query = "SELECT idcontact, name, surname, birthday FROM contact WHERE state = 1 AND username = %s AND MONTH(birthday) = %s"
             values = (username, fecha_actual)
             context1.mycursor.execute(query,values)
             contactsDB = context1.mycursor.fetchall()
         contacts = []
-        for contactDB in contactsDB:
-            contact = Contact()
-            contact.id = contactDB[0]
-            contact.name = contactDB[1]
-            contact.surname = contactDB[2]
-            contact.email = contactDB[3]
-            contact.fechaNacimiento = contactDB[7]
-            contacts.append(contact)
-        return contacts
+        if len(contactsDB) == 0:
+            salir = False
+            
+        else:
+            for contactDB in contactsDB:
+                contact = Contact()
+                contact.id = contactDB[0]
+                contact.name = contactDB[1]
+                contact.surname = contactDB[2]
+                date_str = contactDB[3] 
+                contact.birthday = date_str.strftime('%d-%m-%Y')
+                contacts.append(contact)
+                salir = True
+        return contacts, salir
+        
     
 
 
     def get_contact(self, contact):
         with Context() as context1:
-            query = "SELECT *, DATE_FORMAT(fechaNacimiento, '%d-%m-%Y') AS fechaNacimiento_invertida FROM contact WHERE idcontact = %s"
+            query = "SELECT * FROM contact WHERE idcontact = %s"
             values = (contact.id,)
             context1.mycursor.execute(query, values)
             contactDB = context1.mycursor.fetchone()
@@ -56,13 +67,17 @@ class ContactRepository:
             contact.email = contactDB[3]
             contact.username = contactDB[4]
             contact.state = contactDB[5]
-            contact.fechaNacimiento = contactDB[7]
+            date_str = contactDB[6] 
+            if not date_str == None:
+                contact.birthday = date_str.strftime('%d-%m-%Y')
+            else:
+                contact.birthday = date_str
         return contact
     
     def add_contact(self, contact):
         with Context() as context1:
-            query = "INSERT INTO contact (name, surname, email, username, state, fechaNacimiento) VALUES (%s, %s, %s, %s, %s, STR_TO_DATE(%s, '%d-%m-%Y'))"
-            values = (contact.name, contact.surname, contact.email, contact.username, contact.state, contact.fechaNacimiento)
+            query = "INSERT INTO contact (name, surname, email, username, state, birthday) VALUES (%s, %s, %s, %s, %s, STR_TO_DATE(%s, '%d-%m-%Y'))"
+            values = (contact.name, contact.surname, contact.email, contact.username, contact.state, contact.birthday)
             context1.mycursor.execute(query, values)
             context1.mydb.commit()
             contact.id = context1.mycursor.lastrowid
@@ -70,8 +85,8 @@ class ContactRepository:
 
     def update_contact(self, contact):
         with Context() as context1:
-            query = "UPDATE contact SET name = %s, surname = %s, fechaNacimiento = STR_TO_DATE(%s, '%d-%m-%Y'), email = %s WHERE idcontact = %s AND username = %s"
-            values = (contact.name, contact.surname, contact.fechaNacimiento, contact.email, contact.id, contact.username)
+            query = "UPDATE contact SET name = %s, surname = %s, birthday = %s, email = %s WHERE idcontact = %s AND username = %s"
+            values = (contact.name, contact.surname, contact.birthday, contact.email, contact.id, contact.username)
             context1.mycursor.execute(query, values)
             context1.mydb.commit()
 
